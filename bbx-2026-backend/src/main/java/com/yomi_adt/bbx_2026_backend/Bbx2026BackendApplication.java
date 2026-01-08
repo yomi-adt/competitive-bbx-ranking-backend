4b6fb3c293635240f037cd48ea8008d11f647080
yomi-adt <nbarlaab@gmail.com>
2026-01-07 20:25:59 -0600

    Spring initializered

---
+++ bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/Bbx2026BackendApplication.java
@@ -0,0 +1,13 @@
+package com.yomi_adt.bbx_2026_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bbx2026BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bbx2026BackendApplication.class, args);
+	}
+
+}
---
+++ bbx-2026-backend/src/test/java/com/yomi_adt/bbx_2026_backend/Bbx2026BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yomi_adt.bbx_2026_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bbx2026BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
