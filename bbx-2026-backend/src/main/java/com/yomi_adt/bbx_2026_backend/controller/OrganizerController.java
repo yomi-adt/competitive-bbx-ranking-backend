f2c149a44ccf5d44dbe92a6b270184fad854ba70
yomi-adt <nbarlaab@gmail.com>
2026-01-07 20:50:17 -0600

    config

--- bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/controller/OrganizerController.java
+++ bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/controller/OrganizerController.java
@@ -1,5 +1,12 @@
 package com.yomi_adt.bbx_2026_backend.controller;
 
-public class OrganizerController {
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@RestController
+public class OrganizerController {
+    @RequestMapping(value = "info", method = RequestMethod.GET)
+    public String info() {
+        return "The application is up...";
+    }
 }
