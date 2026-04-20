77b6455db8e2a5cf9f657234fd1c9886b03241ab
yomi-adt <nbarlaab@gmail.com>
2026-04-20 16:49:30 -0500

    Updated SQLite Dialect to Hibernate 6

--- bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/config/SQLiteDialect.java
+++ bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/config/SQLiteDialect.java
@@ -1,36 +1,40 @@
 package com.yomi_adt.bbx_2026_backend.config;
 
 import org.hibernate.dialect.Dialect;
-import org.hibernate.dialect.function.StandardSQLFunction;
-import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.dialect.function.CommonFunctionFactory;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolver;
 
 public class SQLiteDialect extends Dialect {
 
     public SQLiteDialect() {
         super();
 
-        // Register functions (Hibernate 6 style)
-        this.registerFunction("lower", new StandardSQLFunction("lower", StandardBasicTypes.STRING));
-        this.registerFunction("upper", new StandardSQLFunction("upper", StandardBasicTypes.STRING));
+        // Register common SQL functions
+        CommonFunctionFactory functionFactory = new CommonFunctionFactory(this);
+        functionFactory.lower();
+        functionFactory.upper();
+        functionFactory.length();
+        functionFactory.concat();
     }
 
     @Override
-    public boolean supportsIdentityColumns() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsTemporaryTables() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsIfExistsBeforeTableName() {
-        return true;
-    }
-
-    @Override
-    public boolean supportsCascadeDelete() {
+    public boolean supportsInsertReturning() {
         return false;
+    }
+
+    @Override
+    public boolean supportsValuesList() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsLimit() {
+        return true;
+    }
+
+    @Override
+    public boolean supportsOffset() {
+        return true;
     }
 }
