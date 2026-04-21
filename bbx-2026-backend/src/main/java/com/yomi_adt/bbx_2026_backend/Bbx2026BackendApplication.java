1e83878765d2e6f79c3ed6e84966ca646c3ec842
yomi-adt <nbarlaab@gmail.com>
2026-04-20 22:59:25 -0500

    Changed WORKDIR in docker file

--- bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/Bbx2026BackendApplication.java
+++ bbx-2026-backend/src/main/java/com/yomi_adt/bbx_2026_backend/Bbx2026BackendApplication.java
@@ -1,11 +1,7 @@
 package com.yomi_adt.bbx_2026_backend;
 
-import java.io.File;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class Bbx2026BackendApplication {
@@ -13,10 +9,4 @@ public class Bbx2026BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(Bbx2026BackendApplication.class, args);
 	}
-
-	@PostConstruct
-	public void init() {
-		new File("/var/data").mkdirs();
-	}
-
 }
